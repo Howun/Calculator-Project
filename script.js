9f1425a4938eebbbaf30c043004d26d969fc30dd
Howun Lam <howunlam96@gmail.com>
2021-11-11 15:22:16 +0000

    complete change in method in Js as previous method did not work

--- script.js
+++ script.js
@@ -1,36 +1,80 @@
-const numBut = document.querySelectorAll(".calculator__buttons__number");
+const numButtons = document.querySelectorAll(".calculator__buttons__number");
+let answerButton = document.querySelector(".calculator__output__answer");
 const operatorButtons = document.querySelectorAll(".calculator__buttons__operation");
 const equalsButton = document.querySelectorAll(".calculator__buttons__equals");
 const clearAllButton = document.querySelectorAll(".calculator__buttons__clearAll");
 const deleteButton = document.querySelectorAll(".calculator__buttons__delete");
-let currentDisplay = document.querySelector(".calculator__output__input");
-let currentResult = document.querySelector(".calculator__output__result");
-
-let numValue = [];
-let opValue = [];
-
-const numList = Array.from(numBut);
-console.log(numList);
-const operatorList = Array.from(operatorButtons);
-console.log(operatorList);
+let currentDisplay = document.querySelector(".calculator__output__display");
 
 
+const valueLength = (numLength) => currentValue.length < numLength;
+const refreshDisplay = (values) => currentDisplay.innerHTML = values;
+const clearDisplay = (clearScreen) => clearAllButton.innerHTML = clearScreen;
 
-for(let i = 0; i < numList.length; i++) {
-  numList[i].addEventListener("click", () => {
-    numValue.push(numList[i].innerHTML)
-    console.log(numValue)
-    currentDisplay.innerHTML = `${numValue.join("")}`
+
+let currentValue = "";
+let operatorValue = "";
+let storedValue = 0;
+let calculatedValue = 0;
+let previousValue = "";
+
+
+// const numList = Array.from(numBut);
+// console.log(numList);
+// const operatorList = Array.from(operatorButtons);
+// console.log(operatorList);
+
+
+
+// for(let i = 0; i < numList.length; i++) {
+//   numList[i].addEventListener("click", () => {
+//     currentValue.push(numList[i].innerHTML)
+//     console.log(currentValue)
+//     currentDisplay.innerHTML = currentValue += numList
+//   })
+// }
+
+numButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if(valueLength(10)) {
+      (currentValue) ? currentValue = event.target.innerHTML : currentValue += event.target.innerHTML;
+      refreshDisplay(currentValue);
+    }
   })
-}
-
-for(let i=0; i < operatorList.length; i++) {
-  operatorList[i].addEventListener("click", () => {
-    
-  console.log("operation button pressed")
-
 })
-}
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    operatorButtons.forEach((button) => {
+      button.classList.remove("current-operator");
+    });
+    button.classList.add("current-operator");
+    operatorValue = event.target.value;
+    console.log("operator pressed");
+    if (currentValue) {
+      storedValue = parseFloat(currentValue);
+      currentValue = ""
+    }
+  });
+});
+
+// for(let i=0; i < operatorList.length; i++) {
+//   operatorList[i].addEventListener("click", () => {
+    
+//   console.log("operation button pressed")
+
+// })
+// }
+
+// equalsButton.addEventListener("click", () => {
+//   console.log(parseFloat(currentValue))
+// })
+
+// clearAllButton.addEventListener("click" () => {
+//   if (clearAllButton.innerHTML === "AC") {
+//     currentDisplay("0");
+//   }
+// })
 
 
 
